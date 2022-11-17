c2f63fc09a2d5ab83fe5cacbf6b6947bd430f34b
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2022-11-17 13:48:45 +0530

    profiles

--- reunion/reunion/urls.py
+++ reunion/reunion/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home-view'),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('post/', include('post.urls', namespace='post')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
